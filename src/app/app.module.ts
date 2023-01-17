2a3198229fa4a026fde435ec3c1279db35b3909d
Yaresko Vlad <yareskofamily@gmail.com>
2023-01-17 21:51:14 +0200

    Подключил Bootstrap, Создал таблицу и делаю форму

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,21 @@ import { CustomersListComponent } from './components/customers-list/customers-li
 import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
 import { CustomersHostComponent } from './components/customers-host/customers-host.component';
 import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		CustomersListComponent,
 		CustomerDetailsComponent,
-		CustomersHostComponent,],
+		CustomersHostComponent,
+	],
 	imports: [
 		BrowserModule,
 		HttpClientModule,
+		ReactiveFormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent],
-
 })
 export class AppModule { }
