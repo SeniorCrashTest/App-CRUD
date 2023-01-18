245f8fb34a5cff4f15242b9d4bb104f7ce077de8
Yaresko Vlad <yareskofamily@gmail.com>
2023-01-18 21:56:55 +0200

    Добавлен createData, добавлена итерация.

--- src/app/components/customer-details/customer-details.component.ts
+++ src/app/components/customer-details/customer-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DEFAULT_CUSTOMER } from 'src/app/shared/data/mock.data';
 import { HttpService } from 'src/app/shared/services/http.service';
+import { CustomerInterface } from 'src/app/shared/types/customer.interface';
+
 
 @Component({
 	selector: 'crud-customer-details',
@@ -10,11 +13,16 @@ import { HttpService } from 'src/app/shared/services/http.service';
 export class CustomerDetailsComponent implements OnInit {
 
 	form!: FormGroup
+	// controls!: { [key: string]: AbstractControl}
 
 	constructor(private httpService: HttpService, private fb: FormBuilder) { }
 
 	ngOnInit(): void {
 		this.initializeForm()
+	}
+
+	onSubmit(): void {
+		this.httpService.createData(this.form.value)
 	}
 
 	private initializeForm(): void {
@@ -24,8 +32,11 @@ export class CustomerDetailsComponent implements OnInit {
 			mobile: ['', [Validators.required, Validators.minLength(8)]],
 			Location: ['', [Validators.required,]],
 		})
+
+		// this.controls = this.form.controls
+		const customer = DEFAULT_CUSTOMER
+		Object.keys(this.form.controls).forEach(
+			key => this.form.controls[key].setValue(customer?.[key as keyof CustomerInterface])
+		);
 	}
-
-
-	onSubmit(): void { }
 }
